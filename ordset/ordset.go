bbcf7867531349110174155827acac37df20e0b7
Emmanuel Odeke <emm.odeke@gmail.com>
2015-12-03 02:49:51 -0700

    ordset: Length() + ValuesSortedByKeys() added

--- ordset/ordset.go
+++ ordset/ordset.go
@@ -1,96 +1,138 @@
 /*
-    Author: Emmanuel Odeke <odeke@ualberta.ca>
-    Description: Data structure that allows for set operations:
-        put, pop, get
-    however in O(1) time.
-    The caveat however is that extra memory is used since under the
-    hood, it consists of a doubly linked list as well as a hashmap.
-    The hashmap is a item-hash to doubly linked list node map.
+   Author: Emmanuel Odeke <odeke@ualberta.ca>
+   Description: Data structure that allows for set operations:
+       put, pop, get
+   however in O(1) time.
+   The caveat however is that extra memory is used since under the
+   hood, it consists of a doubly linked list as well as a hashmap.
+   The hashmap is a item-hash to doubly linked list node map.
 */
 
 package ordset
 
 import (
-    "github.com/odeke-em/goht/dnode"
+	"sort"
+	"sync"
+
+	"github.com/odeke-em/goht/dnode"
 )
 
 type OrdSet struct {
-    dl *dnode.DNode
-    jar map[uint] *dnode.DNode
+	dl  *dnode.DNode
+	jar map[uint]*dnode.DNode
+	sync.Mutex
 }
 
 func New() *OrdSet {
-    od := new(OrdSet)
-    od.dl = dnode.New(nil)
-    od.jar = map[uint] *dnode.DNode {}
+	od := new(OrdSet)
+	od.dl = dnode.New(nil)
+	od.jar = map[uint]*dnode.DNode{}
 
-    return od
+	return od
+}
+
+func (o *OrdSet) Length() int {
+	o.Lock()
+	defer o.Unlock()
+	return len(o.jar)
+}
+
+type uintSlice []uint
+
+func (u uintSlice) Len() int {
+	return len(u)
+}
+
+func (u uintSlice) Less(i, j int) bool {
+	return u[i] < u[j]
+}
+
+func (u uintSlice) Swap(i, j int) {
+	u[i], u[j] = u[j], u[i]
+}
+
+func (o *OrdSet) ValuesOrderedByKeys() (values []interface{}) {
+	keys := make([]uint, 0, len(o.jar))
+	for key, _ := range o.jar {
+		keys = append(keys, key)
+	}
+
+	sort.Sort(uintSlice(keys))
+
+	values = make([]interface{}, 0, len(keys))
+	for _, key := range keys {
+		if value, ok := o.Get(key); ok {
+			values = append(values, value)
+		}
+	}
+
+	return values
 }
 
 func (o *OrdSet) getDNode(h uint) *dnode.DNode {
-    if o.jar == nil {
-        return nil
-    } else {
-        retr, _ := o.jar[h]
-        return retr
-    }
+	if o.jar == nil {
+		return nil
+	} else {
+		retr, _ := o.jar[h]
+		return retr
+	}
 }
 
 func (o *OrdSet) Get(h uint) (interface{}, bool) {
-    if o.jar == nil {
-        return nil, false
-    } else {
-        retr, check := o.jar[h]
-        if check == false || retr == nil{
-            return nil, false
-        } else {
-            return retr.Value, true
-        }
-    }
+	if o.jar == nil {
+		return nil, false
+	} else {
+		retr, check := o.jar[h]
+		if check == false || retr == nil {
+			return nil, false
+		} else {
+			return retr.Value, true
+		}
+	}
 }
 
 func (o *OrdSet) putIntoJar(k uint, v *dnode.DNode) {
-    o.jar[k] = v
+	o.jar[k] = v
 }
 
 func (o *OrdSet) Add(h uint, item interface{}) (vacated interface{}, fIns bool) {
-    vacated = nil
-    fIns = true // First time insert
+	vacated = nil
+	fIns = true // First time insert
 
-    retr := o.getDNode(h)
-    if retr == nil { // Time to add it
-        nd := o.dl.Prepend(item)
-        o.putIntoJar(h, nd)
-    } else { // Update that value
-        vacated = retr.SetValue(item)
-        fIns = false
-    }
+	retr := o.getDNode(h)
+	if retr == nil { // Time to add it
+		nd := o.dl.Prepend(item)
+		o.putIntoJar(h, nd)
+	} else { // Update that value
+		vacated = retr.SetValue(item)
+		fIns = false
+	}
 
-    return
+	return
 }
 
 func (o *OrdSet) Remove(h uint) (vacated interface{}, exists bool) {
-    retr := o.getDNode(h)
+	retr := o.getDNode(h)
 
-    if retr == nil {
-        return nil, false 
-    } else {
-        prev := retr.GetPrev()
-        next := retr.GetNext()
+	if retr == nil {
+		return nil, false
+	} else {
+		prev := retr.GetPrev()
+		next := retr.GetNext()
 
-        if prev != nil {
-            prev.SetNext(next)
-        }
+		if prev != nil {
+			prev.SetNext(next)
+		}
 
-        if next != nil {
-            next.SetPrev(prev)
-        }
+		if next != nil {
+			next.SetPrev(prev)
+		}
 
-        delete(o.jar, h)
-        return retr.Value, true
-    }
+		delete(o.jar, h)
+		return retr.Value, true
+	}
 }
 
 func (o *OrdSet) Iterator() *dnode.DNode {
-    return o.dl
+	return o.dl
 }
--- ordset/ordset_test.go
+++ ordset/ordset_test.go
@@ -1,62 +1,91 @@
 package ordset
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestInitialization(t *testing.T) {
-    od := New()
-    if od == nil {
-    }
+	od := New()
+	if od == nil {
+	}
 
-    if od.dl == nil {
-        t.Errorf("Head node cannot be nil")
-    }
+	if od.dl == nil {
+		t.Errorf("Head node cannot be nil")
+	}
 
-    if od.jar == nil {
-        t.Errorf("Jar cannot be nil")
-    }
+	if od.jar == nil {
+		t.Errorf("Jar cannot be nil")
+	}
 }
 
 func TestInsertion(t *testing.T) {
-    attrMap := map[uint]interface{} {
-        100: "Amber", 2938: "tribe",
-        94585: "iron", 4586: nil, 193851: t,
-    }
+	attrMap := map[uint]interface{}{
+		100: "Amber", 2938: "tribe",
+		94585: "iron", 4586: nil, 193851: t,
+	}
 
-    od := New()
-    for h, v := range attrMap {
-        vac, isFirstInsert := od.Add(h, v)
-        if isFirstInsert != true {
-            t.Errorf("Expected a unique insert, vacated: %v", vac)
-        }
-    }
+	od := New()
+	for h, v := range attrMap {
+		vac, isFirstInsert := od.Add(h, v)
+		if isFirstInsert != true {
+			t.Errorf("Expected a unique insert, vacated: %v", vac)
+		}
+	}
 
-    // Now doing second phase of insertions
-    for h, v := range attrMap {
-        vac, isFirstInsert := od.Add(h, v)
-        if isFirstInsert != false {
-            t.Errorf("Expected a unique insert, vacated: %v", vac)
-        }
-    }
+	// Now doing second phase of insertions
+	for h, v := range attrMap {
+		vac, isFirstInsert := od.Add(h, v)
+		if isFirstInsert != false {
+			t.Errorf("Expected a unique insert, vacated: %v", vac)
+		}
+	}
 
 }
 
 func TestRemove(t *testing.T) {
-    elemMap := map[uint]interface{} {
-        999: "ignot", 1820: "feng",
-        1924: "Ambivalent", 28457: 2948.18,
-    }
+	elemMap := map[uint]interface{}{
+		999: "ignot", 1820: "feng",
+		1924: "Ambivalent", 28457: 2948.18,
+	}
 
-    od := New()
-    for h, v := range elemMap {
-        od.Add(h, v)
-    }
+	od := New()
+	for h, v := range elemMap {
+		od.Add(h, v)
+	}
 
-    for h, _ := range elemMap {
-        _, existed := od.Remove(h)
-        if existed != true {
-            t.Errorf("Should have been vacated")
-        }
-    }
+	for h, _ := range elemMap {
+		_, existed := od.Remove(h)
+		if existed != true {
+			t.Errorf("Should have been vacated")
+		}
+	}
+}
+
+func TestValuesOrderedByKeys(t *testing.T) {
+	max := uint(100)
+	implicitlySortedValues := make([]uint, 0, max)
+	od := New()
+
+	for i := uint(0); i < max; i++ {
+		od.Add(i, i)
+		implicitlySortedValues = append(implicitlySortedValues, i)
+	}
+
+	if expected, got := len(implicitlySortedValues), od.Length(); expected != got {
+		t.Errorf("expected length %v got %v", expected, got)
+	}
+
+	valuesSortedByKeys := od.ValuesOrderedByKeys()
+
+	if expected, got := len(implicitlySortedValues), len(valuesSortedByKeys); expected != got {
+		t.Errorf("expected length %v got %v", expected, got)
+	}
+
+	// Element by element comparison since []uint != []inteface{}
+	for i, impValue := range implicitlySortedValues {
+		retr := valuesSortedByKeys[i]
+		if retr != impValue {
+			t.Errorf("element %v:: %v != %v", i, retr, impValue)
+		}
+	}
 }
